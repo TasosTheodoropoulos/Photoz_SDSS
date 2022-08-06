b4ee1ed53f8f56552d01fb9edbacdcc17beeab9c
TasosTheodoropoulos <83231945+TasosTheodoropoulos@users.noreply.github.com>
2022-08-06 14:55:02 +0200

    Add files via upload

---
+++ pasq.py
@@ -0,0 +1,42 @@
+import numpy as np
+from  my_lib_03 import *
+from keras.models import load_model
+import gc
+
+
+file_path = "/sps/lsst/users/atheodor/sdss.npz" 
+file = np.load(file_path)
+images = file["cube"]
+labels = file["labels"]
+z = labels["z"]
+der = labels["dered_petro_r"]
+pr = labels["primtarget"]
+dec = labels["dec"]
+ra = labels["ra"]
+
+
+i=0
+pasq = []
+z1 = []
+der1 = []
+dec1 = []
+ra1 = []
+
+for a in images:
+    if ((z[i] <= 0.4) and (der[i] <=17.8) and (pr[i]>=64)) :
+        z1.append(z[i])
+        der1.append(der[i])
+        dec1.append(dec[i])
+        ra1.append(ra[i])
+        i += 1
+    else:
+        i +=1
+
+#Dict = {'z': z1, 'der_petr_r': der1, 'dec': dec1,'ra':ra1}
+np.savez("/sps/lsst/users/atheodor/pasq_03.npz", images = z1,label = der1)
+np.savez("/sps/lsst/users/atheodor/pasq_04.npz", images = dec1,label = ra1)
+
+
+
+
+
