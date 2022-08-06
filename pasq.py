9beb5bfecdaa2679ae54cf5e77e838086b20bebb
TasosTheodoropoulos <83231945+TasosTheodoropoulos@users.noreply.github.com>
2022-08-06 15:37:44 +0200

    Update pasq.py

--- pasq.py
+++ pasq.py
@@ -2,7 +2,10 @@ import numpy as np
 from  my_lib_03 import *
 from keras.models import load_model
 import gc
-
+"""
+This file produces the dataset used in Pasquet et al. given the raw dataset.
+(Needs a lot of available RAM, double the size of the dataset)
+"""
 
 file_path = "/sps/lsst/users/atheodor/sdss.npz" 
 file = np.load(file_path)
@@ -33,8 +36,11 @@ for a in images:
         i +=1
 
 #Dict = {'z': z1, 'der_petr_r': der1, 'dec': dec1,'ra':ra1}
-np.savez("/sps/lsst/users/atheodor/pasq_03.npz", images = z1,label = der1)
-np.savez("/sps/lsst/users/atheodor/pasq_04.npz", images = dec1,label = ra1)
+# Pasquet dataset (images, redshift)
+np.savez("/sps/lsst/users/atheodor/pasq_01.npz", images = images,label = z1)
+
+#np.savez("/sps/lsst/users/atheodor/pasq_03.npz", images = z1,label = der1)
+#np.savez("/sps/lsst/users/atheodor/pasq_04.npz", images = dec1,label = ra1)
 
 
 
